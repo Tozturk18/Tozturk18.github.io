02e6d2d8b00e8950be8fc9b1d4677792ac222f1b
tozturk18 <tunaozturk2001@hotmail.com>
2022-12-08 23:05:20 +0300

    added touch Support

--- DomMesh.js
+++ DomMesh.js
@@ -146,7 +146,32 @@ class DomMesh extends Mesh {
         const raycaster = this._raycaster;
         var target = this;
 
+        window.addEventListener( 'touchstart', (event) => {
+
+            event.preventDefault();
+
+            // calculate pointer position in normalized device coordinates
+            // (-1 to +1) for both components
+            const pointer = new Vector2( Math.round( ( ( event.touches[0].clientX / document.querySelector("canvas").width ) * 2 - 1 )*1000 )/1000, Math.round(( - ( event.touches[0].clientY / document.querySelector("canvas").height ) * 2 + 1 )*1000)/1000 );
+    
+            // update the picking ray with the camera and pointer position
+            raycaster.setFromCamera( pointer, camera );
+    
+            // calculate objects intersecting the picking ray
+            var selected = raycaster.intersectObject( target )[0] || [];
+
+            document.querySelector("h2").textContent = "X: " + pointer.x + "\nY: " + pointer.y;
+
+            // Check if the object under the mouse is this DomMesh object
+            if (selected.object == target) {
+                func();
+            }
+
+        } );
+
         window.addEventListener( 'click', (event) => {
+
+            event.preventDefault();
 
             // calculate pointer position in normalized device coordinates
             // (-1 to +1) for both components
